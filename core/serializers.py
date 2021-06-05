0ed5dd4fc4df51cf9c8bd8f172d3e38bcc652157
Elaine Kroth <any.kroth@gmail.com>
2021-06-05 15:01:25 -0300

    inserindo comentario na view de publicacao

--- core/serializers.py
+++ core/serializers.py
@@ -60,7 +60,7 @@ class InteracaoSerializers(serializers.ModelSerializer):
 class PublicacaoSerializers(serializers.ModelSerializer):
     atividade = AtividadeSerializers(many=True, read_only=True)
     midia = MidiaSerializers(many=True, read_only=True)
-    # comentario = ComentarioSerializers(many=True, read_only=True)
+    comentario = ComentarioSerializers(many=True, read_only=True)
     # interacao = InteracaoSerializers(many=True, read_only=True)
 
     class Meta:
