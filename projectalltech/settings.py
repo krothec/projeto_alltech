242e59e0b61158d9e395c868344e9a536b51b05a
Elaine Kroth <any.kroth@gmail.com>
2021-06-16 19:51:58 -0300

    whitenoise

--- projectalltech/settings.py
+++ projectalltech/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -145,7 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA_ROOT = str(BASE_DIR / 'media')
 # STATIC_ROOT = str(BASE_DIR / 'staticfiles')
