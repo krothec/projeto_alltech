ad19491e598f9b1fd8fb8c09c6f685e6f1662f9a
Elaine Kroth <any.kroth@gmail.com>
2021-06-16 19:43:12 -0300

    ajuste em seeting.py

--- projectalltech/settings.py
+++ projectalltech/settings.py
@@ -139,10 +139,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = str(BASE_DIR / 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+# MEDIA_ROOT = str(BASE_DIR / 'media')
+# STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
